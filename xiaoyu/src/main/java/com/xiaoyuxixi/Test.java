d250ab752ddecf0ec39e2ecd8c1d8a4029127c8a
yuxi <674440302@qq.com>
2020-08-14 23:25:07 +0800

    init

---
+++ xiaoyu/src/main/java/com/xiaoyuxixi/Test.java
@@ -0,0 +1,15 @@
+package com.xiaoyuxixi;
+
+/**
+ * @author : xiaoyu
+ * @version V1.0
+ * @Project: xiaoyuxixi-algorithm
+ * @Package com.xiaoyuxixi
+ * @Description: TODO
+ * @date Date : 2020年08月14日 23:23
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello algorithm....");
+    }
+}
